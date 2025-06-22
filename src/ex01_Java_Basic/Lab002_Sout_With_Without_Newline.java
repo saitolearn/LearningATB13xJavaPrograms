8b3dbf37ff3e4c2da5d86dbaaefc231aaacbeba4
Sai Sowjanya Ch <saitolearn94@gmail.com>
2025-06-23 00:00:28 +0530

    Updated sout code

---
+++ src/ex01_Java_Basic/Lab002_Sout_With_Without_Newline.java
@@ -0,0 +1,19 @@
+package ex01_Java_Basic;
+
+public class Lab002_Sout_With_Without_Newline {
+
+    public static void main(String[] args) {
+        //Print multiple lanes with multiple sout
+        System.out.println("Hi");
+        System.out.println("My name is Sai");
+        System.out.println("Am learning Java Basics");
+
+        //Print multiple lanes with single sout
+        System.out.println("\n Hi \n My name is Sai \n Am learning Java Basics");
+
+
+    }
+
+
+
+}

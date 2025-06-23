9e12e5f315e1cdaf3eae476ab81d8555c07fbed5
Sai Sowjanya Ch <saitolearn94@gmail.com>
2025-06-23 07:01:38 +0530

    updated code in comments.java

--- src/ex01_Java_Basic/Lab004_Comments.java
+++ src/ex01_Java_Basic/Lab004_Comments.java
@@ -3,11 +3,16 @@ package ex01_Java_Basic;
 public class Lab004_Comments {
 
     public static void main(String[] args) {
-        // This is a comment, this code will nto be executed.
-         /*System.out.println("Hi");
+        // This is a Single line comment, this code will not be executed.
+
+
+         /* This is a Multiple Line comments, this code will not be executed.
+         System.out.println("Hi");
          System.out.println("Bye");
          System.out.println("Drink");
          System.out.println("Chai");*/
+
+        System.out.println("Example of Comments explained above.");
     }
 
 

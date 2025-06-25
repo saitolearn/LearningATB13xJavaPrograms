b52151d4048b00552e00b11a3b293392c98da647
Sai Sowjanya Ch <saitolearn94@gmail.com>
2025-06-25 22:53:19 +0530

    added few program

--- src/Java_Assessment_65QnA_60mins/CodingChallenge3.java
+++ src/Java_Assessment_65QnA_60mins/CodingChallenge3.java
@@ -1,4 +1,18 @@
 package Java_Assessment_65QnA_60mins;
 
 public class CodingChallenge3 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Printing all results in one line as per the required format
+        System.out.print("a == b: " + (a == b) + " ");
+        System.out.print("a > b: " + (a > b) + " ");
+        System.out.print("a < b: " + (a < b) + " ");
+        System.out.print("(a > b) && (a > 0): " + ((a > b) && (a > 0)) + " ");
+        System.out.print("(a < b) || (a > 0): " + ((a < b) || (a > 0)) + " ");
+        System.out.print("!(a > b): " + (!(a > b)));
+    }
+
 }

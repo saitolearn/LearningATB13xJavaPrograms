fc589264261f7722ce5c85c9a79858983e58dd1e
Sai Sowjanya Ch <saitolearn94@gmail.com>
2025-06-26 07:36:41 +0530

    program code is updated

---
+++ src/JavaStringsAssessment/Strings_CodingChallenge2.java
@@ -0,0 +1,29 @@
+package JavaStringsAssessment;
+
+import java.util.Scanner;
+
+public class Strings_CodingChallenge2 {
+
+    public static void main(String[] args) {
+
+        // Declare three strings
+        String string1 = "Hello";
+        String string2 = "hello";
+        String string3 = "Hello";
+
+        // Compare using equals()
+        boolean equalsResult = string1.equals(string2);
+
+        // Compare using equalsIgnoreCase()
+        boolean equalsIgnoreCaseResult = string1.equalsIgnoreCase(string2);
+
+        // Compare using compareTo()
+        int compareToResult = string1.compareTo(string2);
+
+        // Print results
+        System.out.println("equals(): " + equalsResult);
+        System.out.println("equalsIgnoreCase(): " + equalsIgnoreCaseResult);
+        System.out.println("compareTo(): " + compareToResult);
+
+    }
+}
